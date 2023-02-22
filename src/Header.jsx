aa472bf9f36d20ded2df1e5bc4c8edb836e52a18
Pavel Petrov <104124867+PashaPP@users.noreply.github.com>
2023-02-22 16:41:12 +0200

    09 lesson

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+  <div>     
+    <a href="#S">CSS</a>
+    <a href="#S">JS</a>
+    <a href="#S">HTML</a>
+    <a href="#S">React</a>       
+  </div>
+  )};
+
+  
