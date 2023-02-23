33a2e111f9756d33e62c6387b4f7c3d51e92ea01
Pavel Petrov <104124867+PashaPP@users.noreply.github.com>
2023-02-23 11:05:34 +0200

    12 lesson

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ export const App = () => {
       <header className='header'>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2CFxacNIJXeP5bv3-HwPBw-vf9pqnxeHbG4QP6cgjuTVO1BMppIM1oMkg66V-E7CBUUw&usqp=CAU" alt="" />
       </header>
-      <nav className='nav'>
-        <div>
-          <a href="#s">Menu</a>
-        </div>
-        <div>
-          <a href="#s">Profile</a>
-        </div>
-        <div>
-          <a href="#s">Messages</a>
-        </div>
-      </nav>
+      <div className='nav'>
+        <nav >
+          <div>
+            <a href="#s">Profile</a>
+          </div>
+          <div>
+            <a href="#s">Messages</a>
+          </div>
+          <div>
+            <a href="#s">News</a>
+          </div>
+          <div>
+            <a href="#s">Music</a>
+          </div>
+          <div>
+            <a href="#s">Settings</a>
+          </div>
+        </nav></div>
       <div className='content'>
-        <div>
-          Main Content
-        </div>
+        <img src="https://c4.wallpaperflare.com/wallpaper/4/285/220/wave-yellow-background-yellow-fon-hd-wallpaper-preview.jpg" alt="oboi" />
       </div>
     </div>
   )
