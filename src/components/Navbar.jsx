8be1bebd812c78988b8cd2d30bab9da70afd5031
Pavel Petrov <104124867+PashaPP@users.noreply.github.com>
2023-02-24 11:12:29 +0200

    1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,23 @@ import n from'./Navbar.module.css'
 export const Navbar = () => {
   return (
       <nav className={n.nav}>
-      <div>
-        <div className={`${n.item} ${n.active}`}>
-          <a>Profile</a>
-        </div>
-        <div className={n.item}>
-          <a>Messages</a>
-        </div>
-        <div className={`${n.item} ${n.active}`}>
-          <a>News</a>
-        </div>
-        <div className={n.item}>
-          <a>Music</a>
-        </div>
-        <div className={n.item}></div>
-          <a>Settings</a>
-        </div>
-      </div>
-      </nav>
+            <div>
+            <div className={`${n.item} ${n.active}`}>
+            <a>Profile</a>
+            </div>
+            <div className={n.item}>
+            <a>Messages</a>
+            </div>
+            <div className={`${n.item} ${n.active}`}>
+            <a>News</a>
+            </div>
+            <div className={n.item}>
+            <a>Music</a>
+            </div>
+            <div className={n.item}></div>
+            <a>Settings</a>
+            </div>
+       </nav>
   )};
 
   
