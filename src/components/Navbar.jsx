ca944f4abdf04d533bfbcbe61b0f37489145a418
Pavel Petrov <104124867+PashaPP@users.noreply.github.com>
2023-02-28 12:01:58 +0200

    15

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,22 @@ export const Navbar = () => {
   return (
       <nav className={n.nav}>
             <div>
-            <div className={`${n.item} ${n.active}`}>
-            <a>Profile</a>
-            </div>
-            <div className={n.item}>
-            <a>Messages</a>
+           <div className={`${n.item} ${n.active}`}>
+               <a>Profile</a>
             </div>
             <div className={`${n.item} ${n.active}`}>
-            <a>News</a>
+               <a>Messages</a>
             </div>
             <div className={n.item}>
-            <a>Music</a>
+               <a>News</a>
             </div>
-            <div className={n.item}></div>
-            <a>Settings</a>
+            <div className={`${n.item} ${n.active}`}>
+               <a>Music</a>
+            </div>
+            <div className={n.item} >
+               <a>Settingus</a>
+            </div>
+               
             </div>
        </nav>
   )};
